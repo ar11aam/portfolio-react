3a42b0b0604aac9fe8fa21b6fe73cf9d5070539b
Arham Tayyab <arhamtayyab553@gmail.com>
2023-09-26 00:45:46 +0500

    :wrench: Few Fixes

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,290 +9,53 @@ const Projects = () => {
           <div className="row">
             <div className="col-lg-12">
               <div className="main_title text-left">
-                <h2>
-                  quality work <br />
-                  Recently done project{" "}
-                </h2>
+                <h2 className="text-center">Recently done project </h2>
               </div>
             </div>
           </div>
-          <div className="filters portfolio-filter">
-            <ul>
-              <li className="active" data-filter="*">
-                all
-              </li>
-              <li data-filter=".popular">popular</li>
-              <li data-filter=".latest"> latest</li>
-              <li data-filter=".following">following</li>
-              <li data-filter=".upcoming">upcoming</li>
-            </ul>
-          </div>
-
-          <div className="filters-content">
-            <div className="row portfolio-grid justify-content-center">
-              <div className="col-lg-4 col-md-6 all latest">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p1.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p1.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>
-                      <a href="portfolio-details.html">minimal design</a>
-                    </h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 all popular">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p2.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p2.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>
-                      <a href="portfolio-details.html">Paint wall</a>
-                    </h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 all latest">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p3.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p3.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>
-                      <a href="portfolio-details.html">female light</a>
-                    </h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 all popular">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p4.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p4.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>
-                      <a href="portfolio-details.html">fourth air</a>
-                    </h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 all following">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p6.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p5.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>
-                      <a href="portfolio-details.html">together sign</a>
-                    </h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 all upcoming">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p5.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p6.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>
-                      <a href="portfolio-details.html">multiply fowl</a>
-                    </h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 all upcoming following">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p7.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p7.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>
-                      <a href="portfolio-details.html">green heaven</a>
-                    </h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 all following">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p8.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p8.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>fly male</h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-4 col-md-6 all upcoming">
-                <div className="portfolio_box">
-                  <div className="single_portfolio">
-                    <img
-                      className="img-fluid w-100"
-                      src="assets/img/portfolio/p9.jpg"
-                      alt=""
-                    />
-                    <div className="overlay"></div>
-                    <a href="assets/img/portfolio/p9.jpg" className="img-gal">
-                      <div className="icon">
-                        <span className="lnr lnr-cross"></span>
-                      </div>
-                    </a>
-                  </div>
-                  <div className="short_info">
-                    <h4>
-                      <Link to="/">season face</Link>
-                    </h4>
-                    <p>Animated, portfolio</p>
-                  </div>
-                </div>
-              </div>
+          <div className="row">
+            <div className="col-lg-6 d-flex justify-content-center">
+              <iframe
+                src="https://www.galactic-marine.com"
+                width={400}
+                height={700}
+                sandbox="allow-scripts allow-modal"
+                title="Custom title"
+              ></iframe>
             </div>
+            <div className="col-lg-6 d-flex justify-content-center main_title text-left">
+              <h1 style={{ color: "#128fc8" }}>
+                <a
+                  href="https://www.galactic-marine.com"
+                  style={{
+                    color: "#128fc8",
+                    textDecoration: "underline",
+                    fontWeight: "700",
+                  }}
+                >
+                  Galactic Pvt. limited
+                </a>
+              </h1>
+            </div>
+          </div>
+          <div
+            className="row d-flex justify-content-center"
+            style={{ marginTop: "80px" }}
+          >
+            <button className="text-center">More</button>
           </div>
         </div>
       </section>
       <section className="features_area section_gap_top">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-lg-8 text-center">
-              <div className="main_title">
-                <h2>service offers </h2>
-                <p>
-                  Is give may shall likeness made yielding spirit a itself
-                  togeth created after sea <br /> is in beast beginning signs
-                  open god you're gathering ithe
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="row feature_inner">
-            <div className="col-lg-3 col-md-6">
-              <div className="feature_item">
-                <img src="assets/img/services/s1.png" alt="" />
-                <h4>Wp developing</h4>
-                <p>
-                  Creeping for female light years that lesser can't evening
-                  heaven isn't bearing tree
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="feature_item">
-                <img src="assets/img/services/s2.png" alt="" />
-                <h4>UI/ux design</h4>
-                <p>
-                  Creeping for female light years that lesser can't evening
-                  heaven isn't bearing tree
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="feature_item">
-                <img src="assets/img/services/s3.png" alt="" />
-                <h4>Web design</h4>
-                <p>
-                  Creeping for female light years that lesser can't evening
-                  heaven isn't bearing tree
-                </p>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="feature_item">
-                <img src="assets/img/services/s4.png" alt="" />
-                <h4>seo optimize</h4>
-                <p>
-                  Creeping for female light years that lesser can't evening
-                  heaven isn't bearing tree
-                </p>
-              </div>
+        <div className="row justify-content-center">
+          <div className="col-lg-8 text-center">
+            <div className="main_title">
+              <h2>service offers </h2>
+              <p>
+                Is give may shall likeness made yielding spirit a itself togeth
+                created after sea <br /> is in beast beginning signs open god
+                you're gathering ithe
+              </p>
             </div>
           </div>
         </div>
